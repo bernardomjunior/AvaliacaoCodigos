130b36f399e60ff75343644836dc6ae884fd9ba9
Bernardo Junior <bernardodemsj@gmail.com>
2021-05-24 00:12:59 -0300

    files

---
+++ knucleotide/android/app/src/main/kotlin/com/example/knucleotide/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.knucleotide
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mandelbrot/lib/Repository.dart
@@ -0,0 +1,22 @@
+import 'package:http/http.dart' as http;
+
+const BASE_URL = "http://192.168.0.100:3000/";
+const START_ENDPOINT = "what_now/";
+const FINISH_ENDPOINT = "logdata/";
+
+void start(Function runCode) async {
+  var response = await http.get(BASE_URL + START_ENDPOINT);
+  if (response.statusCode == 200){
+    print(response.body.toString());
+    runCode();
+  }else {
+    print("Error on starting code");
+  }
+}
+
+void endCode() async {
+  var response = await http.get(BASE_URL + FINISH_ENDPOINT);
+  if (response.statusCode != 200){
+    print("Error on logging battery status code");
+  }
+}
