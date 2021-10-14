086a81b9387c551bb9d608d73298fdddebee66a1
Bernardo <bernardodemsj@gmail.com>
2021-10-14 01:35:02 -0300

    updates to knucleotide project
    new java version to 11, adds fastutil package and parameter folder

--- Android/knucleotide/app/src/main/java/com/android/knucleotide/MainActivity.java
+++ Android/knucleotide/app/src/main/java/com/android/knucleotide/MainActivity.java
@@ -3,6 +3,11 @@ package com.android.knucleotide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String text;
+        Log.i("CODE-RUN", "START");
+        try {
+            text = readAssets();
+            boolean a = false;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.i("CODE-RUN", "END");
     }
+
+    private String readAssets() throws IOException {
+        StringBuilder completeFile = new StringBuilder();
+        BufferedReader reader = null;
+        reader = new BufferedReader(
+                    new InputStreamReader(getAssets().open("knucleotide input - 2500000.txt")));
+        String mLine;
+        while ((mLine = reader.readLine()) != null) {
+            completeFile.append(mLine);
+        }
+        reader.close();
+        return completeFile.toString();
+    }
+
+
 }
